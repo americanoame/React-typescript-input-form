d43095064cd9a468c999f41b9062a301819acdb6
Americano.Dev <andrenations@gmail.com>
2024-08-05 19:59:14 -0400

    Create a polymorphic Container component with improved type safety

--- input-form/src/components/Container.tsx
+++ input-form/src/components/Container.tsx
@@ -1,13 +1,11 @@
-import { type ElementType } from "react"
+import { ComponentPropsWithoutRef, type ElementType } from 'react';
 
-type ContainerProps = {
-    as: ElementType
+type ContainerProps<T extends ElementType> = {
+  as?: T;
+  children: React.ReactNode;
+} & ComponentPropsWithoutRef<T>;
+
+export default function Container<C extends ElementType>({ as, children, ...props }: ContainerProps<C>) {
+  const Component = as || 'div';
+  return <Component {...props}>{children}</Component>;
 }
-
-
-
-export default function Container({as}: ContainerProps) {
-    const Component = as
-    return <Component />
-} 
-
