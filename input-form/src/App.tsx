7fd2945645f538b7669f7678c6499276b793f97e
Americano.Dev <andrenations@gmail.com>
2024-08-29 13:41:12 -0400

    Add Input component with forwardRef and useRef in App component for ref handling

--- input-form/src/App.tsx
+++ input-form/src/App.tsx
@@ -1,13 +1,14 @@
-import Button from "./components/Button";
-import Container from "./components/Container";
+import { useRef } from 'react';
+import Input from './components/Input.tsx';
 
 function App() {
-  
+  const input = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-     <Container as={Button}>Click</Container>
+      <Input label="test" id="test" ref={input} />
     </main>
   );
 }
 
-export default App
+export default App;
