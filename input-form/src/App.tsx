5f2314f98f9a9c99ad6ff1f1829937f2efb72094
Americano.Dev <andrenations@gmail.com>
2024-08-29 15:28:39 -0400

    Add Form component with Input fields and Save button in App component

--- input-form/src/App.tsx
+++ input-form/src/App.tsx
@@ -1,12 +1,24 @@
-import { useRef } from 'react';
+// import { useRef } from 'react';
 import Input from './components/Input.tsx';
+import Form from './components/Form.tsx';
+import Button from './components/Button.tsx';
+
+// This code defines a simple React app where a Form component 
+// contains two Input fields (for name and age) and a Button to 
+// submit the form. The Form component wraps the Input components 
+// and the Button for layout and structure.
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
-
   return (
     <main>
-      <Input label="test" id="test" ref={input} />
+      <Form>
+        <Input type='text' label='name' id='name' />
+        <Input type='number' label='age' id='age'></Input>
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
+      
     </main>
   );
 }
