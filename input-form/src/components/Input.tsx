9bc17a0324d2b6e77c516ca0410ca97c7d1838a9
Americano.Dev <andrenations@gmail.com>
2024-08-30 23:37:52 -0400

    Add label and ref forwarding to Input component

--- input-form/src/components/Input.tsx
+++ input-form/src/components/Input.tsx
@@ -1,16 +1,24 @@
 import { forwardRef, type ComponentPropsWithRef } from "react";
 
 type InputProps = {
-  label: string;
-  id: string;
-} & ComponentPropsWithRef<"input">;
+  label: string;    // Custom prop for the label text
+  id: string;       // Custom prop for the input's id attribute
+} & ComponentPropsWithRef<"input">;  // Merging custom props with the standard props of an <input> element
 
+
+//forwardRef: This function is used to forward a ref from a parent component to a child
+
+// Using forwardRef to forward the ref to the input element
 export default forwardRef<HTMLInputElement, InputProps>(function Input(
   { label, id, ...props }, ref) {
   return (
     <p>
-      <label htmlFor={id}>{label}</label>
-      <input id={id} {...props} ref={ref} />
+      {/* Rendering the label and input element */}
+      <label htmlFor={id}>{label}</label>   
+
+      {/* Using the spread operator to pass the props to the input element */}
+      <input id={id} {...props} ref={ref} />   {/* Forwarding the ref to the input element */}
+      
     </p>
   );
 });
