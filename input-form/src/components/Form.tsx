68cf0f49838130c73ed1fa98f1d79b361fbc42ea
Americano.Dev <andrenations@gmail.com>
2024-09-05 15:33:25 -0400

    added form submission logic and shared data handling in Form component

--- input-form/src/components/Form.tsx
+++ input-form/src/components/Form.tsx
@@ -1,10 +1,21 @@
-import {  ComponentPropsWithoutRef } from 'react';
+import { ComponentPropsWithoutRef, FormEvent } from 'react';
 
-type FormProps = ComponentPropsWithoutRef<'form'>;
+type FormProps = ComponentPropsWithoutRef<'form'> & {
+  onSave: (value: unknown) => void;
+};
 
+export default function Form({onSave, children, ...otherProps}: FormProps) {
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
 
+    const forData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(forData);
+    onSave(data);
+  }
 
-export default function Form(props: FormProps) {
-  return<form {...props}>{props.children}</form>;
+  return (
+    <form onSubmit={handleSubmit} {...otherProps}>
+      {children}
+    </form>
+  );
 }
-
